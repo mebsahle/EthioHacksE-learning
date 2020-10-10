42731e29536f508fd80206658e2a6f1636b7766a
Mebatsion Sahle <51055556+mebsahle@users.noreply.github.com>
2020-10-10 10:41:08 +0300

    Update settings.py

--- elearning/settings.py
+++ elearning/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'vfnox%6)63=)j^a18%1@_(o)5#co%4+a0hivoy#ykvgshqhor+'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
